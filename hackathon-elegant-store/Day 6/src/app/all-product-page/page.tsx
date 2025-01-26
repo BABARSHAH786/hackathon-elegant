0985dd45fccaa45891bfe32faa835a30e6ccc3c6
Syed Babar Ali <159919873+BABARSHAH786@users.noreply.github.com>
2025-01-26 13:30:48 +0500

    updte project

---
+++ hackathon-elegant-store/Day 6/src/app/all-product-page/page.tsx
@@ -0,0 +1,457 @@
+import Image from "next/image";
+import Link from "next/link";
+import SidebrPrdAll from "../components/SidebrPrdAll";
+import {  FaCaretDown, FaFilter } from "react-icons/fa";
+export default function AllProduct() {
+    return (
+      <>
+      <div className="flex">
+      <p className="ml-12 text-[24px ]  font-bold flex ">New (500)</p>
+        <span className="flex ml-[900px] ">Hide Filters
+          <span><FaFilter /></span>
+
+          <span className="flex">Sort By
+            <span><FaCaretDown  /></span>
+            
+          </span>
+        </span>
+        </div>
+        <div className="max-ss:h-auto max-ss:w-[391px] w-[1440px ] max-ss:grid  flex ">
+        <div className="">
+        <SidebrPrdAll />
+
+        </div>
+        <div className="max-ss:w-auto w-[1092px ] flex flex-wrap ">
+
+
+        
+          
+
+           
+           {/****************************  prd original */}
+           <div className="flex flex-wrap">
+           <div className="w-[348px ] h-[533px] ml-12 flex  mt-12 gap-4 ">
+           <div className="w-[348px ] h-[510px]">
+           <Image
+          className=""
+          src={"/Rectangle.png"}
+          alt="shoes"
+          width={348}
+          height={348}
+        />
+        <div className="w-[348px ] h-[185px] flex mt-6">
+        <div className=" h-[134px]">
+          <p className="font-semibold text-[#9E3500] ">Just In</p>
+          <p className="font-medium">Nike Air Force 1 Mid &apos;07</p>
+          <div>
+          <p className="font-semibold text-[#757575] ">Men&apos;s Shoes</p>
+          <p className="font-semibold text-[#757575] ">1 Colour</p>
+          </div>
+          <p className="font-semibold  ">MRP : ₹ 10 795.00</p>
+
+          </div>
+
+          </div>
+
+
+</div>
+
+
+</div>
+
+
+           {/* second */}
+           <div className="w-[348px ] h-[533px] ml-12 flex mt-12 gap-4 ">
+           <div className="w-[348px ] h-[510px]">
+           <Image
+          className=""
+          src={"/Rectangle (1).png"}
+          alt="shoes"
+          width={348}
+          height={348}
+        />
+        <div className="w-[348px ] h-[185px] flex mt-6">
+        <div className=" h-[134px]">
+          <p className="font-semibold text-[#9E3500] ">Just In</p>
+          <p className="font-medium">Nike Air Force 1 Mid &apos;07</p>
+          <div>
+          <p className="font-semibold text-[#757575] ">Men&apos;s Shoes</p>
+          <p className="font-semibold text-[#757575] ">1 Colour</p>
+          </div>
+          <p className="font-semibold  ">MRP : ₹ 4 995.00</p>
+
+          </div>
+
+          </div>
+
+
+</div>
+</div>
+
+{/* 3rd */}
+<div className="w-[348px ] h-[533px] ml-12 flex mt-12 gap-4 ">
+           <div className="w-[348px ] h-[510px]">
+           <Link className=" " href="/product-detail">
+
+           <Image
+          className=""
+          src={"/Rectangle (2).png"}
+          alt="shoes"
+          width={348}
+          height={348}
+        />
+           </Link>
+        <div className="w-[348px ] h-[185px] flex mt-6">
+        <div className=" h-[134px]">
+          <p className="font-semibold text-[#9E3500] ">Just In</p>
+          <p className="font-medium">Nike Air Force 1 Mid &apos;07</p>
+          <div>
+          <p className="font-semibold text-[#757575] ">Men&apos;s Shoes</p>
+          <p className="font-semibold text-[#757575] ">1 Colour</p>
+          </div>
+          <p className="font-semibold  ">MRP : ₹ 10 795.00</p>
+
+          </div>
+
+          </div>
+
+
+</div>
+</div>
+</div>
+
+{/* row 2 */}
+<div className="flex flex-wrap">
+           <div className="w-[348px ] h-[533px] ml-12 flex  mt-12 gap-4 ">
+           <div className="w-[348px ] h-[510px]">
+           <Image
+          className=""
+          src={"/Rectangle (3).png"}
+          alt="shoes"
+          width={348}
+          height={348}
+        />
+        <div className="w-[348px ] h-[185px] flex mt-6">
+        <div className=" h-[134px]">
+          <p className="font-semibold text-[#9E3500] ">Just In</p>
+          <p className="font-medium">Nike Air Force 1 Mid &apos;07</p>
+          <div>
+          <p className="font-semibold text-[#757575] ">Men&apos;s Shoes</p>
+          <p className="font-semibold text-[#757575] ">1 Colour</p>
+          </div>
+          <p className="font-semibold  ">MRP : ₹ 10 795.00</p>
+
+          </div>
+
+          </div>
+
+
+</div>
+
+
+</div>
+
+
+           {/* second */}
+           <div className="w-[348px ] h-[533px] ml-12 flex mt-12 gap-4 ">
+           <div className="w-[348px ] h-[510px]">
+           <Image
+          className=""
+          src={"/Rectangle (4).png"}
+          alt="shoes"
+          width={348}
+          height={348}
+        />
+        <div className="w-[348px ] h-[185px] flex mt-6">
+        <div className=" h-[134px]">
+          <p className="font-semibold text-[#9E3500] ">Just In</p>
+          <p className="font-medium">Nike Air Force 1 Mid &apos;07</p>
+          <div>
+          <p className="font-semibold text-[#757575] ">Men&apos;s Shoes</p>
+          <p className="font-semibold text-[#757575] ">1 Colour</p>
+          </div>
+          <p className="font-semibold  ">MRP : ₹ 10 795.00</p>
+
+          </div>
+
+          </div>
+
+
+</div>
+</div>
+
+{/* 3rd */}
+<div className="w-[348px ] h-[533px] ml-12 flex mt-12 gap-4 ">
+           <div className="w-[348px ] h-[510px]">
+           <Link className=" " href="/product-detail">
+
+           <Image
+          className=""
+          src={"/Rectangle (5).png"}
+          alt="shoes"
+          width={348}
+          height={348}
+        />
+           </Link>
+        <div className="w-[348px ] h-[185px] flex mt-6">
+        <div className=" h-[134px]">
+          <p className="font-semibold text-[#9E3500] ">Just In</p>
+          <p className="font-medium">Nike Air Force 1 Mid &apos;07</p>
+          <div>
+          <p className="font-semibold text-[#757575] ">Men&apos;s Shoes</p>
+          <p className="font-semibold text-[#757575] ">1 Colour</p>
+          </div>
+          <p className="font-semibold  ">MRP : ₹ 10 795.00</p>
+
+          </div>
+
+          </div>
+
+
+</div>
+</div>
+</div>
+
+
+{/* 3rd row */}
+<div className="flex flex-wrap">
+           <div className="w-[348px ] h-[533px] ml-12 flex  mt-12 gap-4 ">
+           <div className="w-[348px ] h-[510px]">
+           <Image
+          className=""
+          src={"/Rectangle (6).png"}
+          alt="shoes"
+          width={348}
+          height={348}
+        />
+        <div className="w-[348px ] h-[185px] flex mt-6">
+        <div className=" h-[134px]">
+          <p className="font-semibold text-[#9E3500] ">Just In</p>
+          <p className="font-medium">Nike Air Force 1 Mid &apos;07</p>
+          <div>
+          <p className="font-semibold text-[#757575] ">Men&apos;s Shoes</p>
+          <p className="font-semibold text-[#757575] ">1 Colour</p>
+          </div>
+          <p className="font-semibold  ">MRP : ₹ 10 795.00</p>
+
+          </div>
+
+          </div>
+
+
+</div>
+
+
+</div>
+
+
+           {/* second */}
+           <div className="w-[348px ] h-[533px] ml-12 flex mt-12 gap-4 ">
+           <div className="w-[348px ] h-[510px]">
+           <Image
+          className=""
+          src={"/Rectangle (7).png"}
+          alt="shoes"
+          width={348}
+          height={348}
+        />
+        <div className="w-[348px ] h-[185px] flex mt-6">
+        <div className=" h-[134px]">
+          <p className="font-semibold text-[#9E3500] ">Just In</p>
+          <p className="font-medium">Nike Air Force 1 Mid &apos;07</p>
+          <div>
+          <p className="font-semibold text-[#757575] ">Men&apos;s Shoes</p>
+          <p className="font-semibold text-[#757575] ">1 Colour</p>
+          </div>
+          <p className="font-semibold  ">MRP : ₹ 10 795.00</p>
+
+          </div>
+
+          </div>
+
+
+</div>
+</div>
+
+{/* 3rd */}
+<div className="w-[348px ] h-[533px] ml-12 flex mt-12 gap-4 ">
+           <div className="w-[348px ] h-[510px]">
+           <Link className=" " href="/product-detail">
+
+           <Image
+          className=""
+          src={"/Rectangle (8).png"}
+          alt="shoes"
+          width={348}
+          height={348}
+        />
+           </Link>
+        <div className="w-[348px ] h-[185px] flex mt-6">
+        <div className=" h-[134px]">
+          <p className="font-semibold text-[#9E3500] ">Just In</p>
+          <p className="font-medium">Nike Air Force 1 Mid &apos;07</p>
+          <div>
+          <p className="font-semibold text-[#757575] ">Men&apos;s Shoes</p>
+          <p className="font-semibold text-[#757575] ">1 Colour</p>
+          </div>
+          <p className="font-semibold  ">MRP : ₹ 10 795.00</p>
+
+          </div>
+
+          </div>
+
+
+</div>
+</div>
+</div>
+
+{/* 4th row */}
+<div className="flex flex-wrap">
+           <div className="w-[348px ] h-[533px] ml-12 flex  mt-12 gap-4 ">
+           <div className="w-[348px ] h-[510px]">
+           <Image
+          className=""
+          src={"/Rectangle (9).png"}
+          alt="shoes"
+          width={348}
+          height={348}
+        />
+        <div className="w-[348px ] h-[185px] flex mt-6">
+        <div className=" h-[134px]">
+          <p className="font-semibold text-[#9E3500] ">Just In</p>
+          <p className="font-medium">Nike Air Force 1 Mid &apos;07</p>
+          <div>
+          <p className="font-semibold text-[#757575] ">Men&apos;s Shoes</p>
+          <p className="font-semibold text-[#757575] ">1 Colour</p>
+          </div>
+          <p className="font-semibold  ">MRP : ₹ 10 795.00</p>
+
+          </div>
+
+          </div>
+
+
+</div>
+
+
+</div>
+
+
+           {/* second */}
+           <div className="w-[348px ] h-[533px] ml-12 flex mt-12 gap-4 ">
+           <div className="w-[348px ] h-[510px]">
+           <Image
+          className=""
+          src={"/Rectangle (10).png"}
+          alt="shoes"
+          width={348}
+          height={348}
+        />
+        <div className="w-[348px ] h-[185px] flex mt-6">
+        <div className=" h-[134px]">
+          <p className="font-semibold text-[#9E3500] ">Just In</p>
+          <p className="font-medium">Nike Air Force 1 Mid &apos;07</p>
+          <div>
+          <p className="font-semibold text-[#757575] ">Men&apos;s Shoes</p>
+          <p className="font-semibold text-[#757575] ">1 Colour</p>
+          </div>
+          <p className="font-semibold  ">MRP : ₹ 10 795.00</p>
+
+          </div>
+
+          </div>
+
+
+</div>
+</div>
+
+{/* 3rd */}
+<div className="w-[348px ] h-[533px] ml-12 flex mt-12 gap-4 ">
+           <div className="w-[348px ] h-[510px]">
+           <Link className=" " href="/product-detail">
+
+           <Image
+          className=""
+          src={"/Rectangle (11).png"}
+          alt="shoes"
+          width={348}
+          height={348}
+        />
+           </Link>
+        <div className="w-[348px ] h-[185px] flex mt-6">
+        <div className=" h-[134px]">
+          <p className="font-semibold text-[#9E3500] ">Just In</p>
+          <p className="font-medium">Nike Air Force 1 Mid &apos;07</p>
+          <div>
+          <p className="font-semibold text-[#757575] ">Men&apos;s Shoes</p>
+          <p className="font-semibold text-[#757575] ">1 Colour</p>
+          </div>
+          <p className="font-semibold  ">MRP : ₹ 10 795.00</p>
+
+          </div>
+
+          </div>
+
+
+</div>
+</div>
+</div>
+
+
+
+{/* last */}
+<div className="w-[1092px ] h-[259px] border-t-[1px] border-[#E5E5E5] ">
+<p className="mt-10 font-medium text-[19px] ">Related Categories</p>
+
+<div className="max-ss:w-auto w-[1092px ] h-[174px] flex flex-wrap ">
+<div className="w-[160.83px] h-[34px] border-[1px] rounded-[20px] mt-6 py-2 px-[5.83] text-[12px] text-center ">Best Selling Products</div>
+
+<div className="w-[160.83px] h-[34px] border-[1px] rounded-[20px] mt-6 py-2 px-[5.83] text-[12px] text-center ">Best Shoes</div>
+
+
+<div className="w-[160.83px] h-[34px] border-[1px] rounded-[20px] mt-6 py-2 px-[5.83] text-[12px] text-center  ">New Basketball Shoes</div>
+
+
+<div className="w-[160.83px] h-[34px]  border-[1px] rounded-[20px] mt-6 py-2 px-[5.83] text-[12px] text-center ">New Football Shoes</div>
+
+
+<div className="w-[160.83px] h-[34px]  text-[12px] text-center   border-[1px] rounded-[20px] mt-6 py-2 px-[5.83px]  ">New Men&apos;s Shoes</div>
+
+
+<div className="w-[160.83px] h-[34px]  text-[12px] text-center  border-[1px] rounded-[20px] mt-6 py-2 px-[5.83] ">New Running Shoes</div>
+
+
+<div className="w-[160.83px] h-[34px] text-[12px] text-center    border-[1px] rounded-[20px] mt-6 py-2 px-[5.83]">Best Men&apos;s Shoes</div><br />
+
+<div className="w-[160.83px] h-[34px]  text-[12px] text-center  border-[1px] rounded-[20px] mt-6 py-2 px-[5.83]">Best Men&apos;s Shoes</div>
+
+
+<div className="w-[160.83px] h-[34px] text-[12px] text-center  border-[1px] rounded-[20px] mt-6 py-2 px-[5.83px] ">Best Men&apos;s Shoes</div>
+
+<div className="w-[160.83px] h-[34px]  text-[12px] text-center  border-[1px] rounded-[20px] mt-6 py-2 px-[5.83px] ">Best Men&apos;s Shoes</div>
+
+</div>
+
+
+
+
+
+
+
+
+</div>
+</div>
+
+
+
+           
+
+
+
+    </div>
+
+
+
+    </>
+    )
+}
---
+++ hackathon-elegant-store/Day 6/src/app/components/Navbar.tsx
@@ -0,0 +1,99 @@
+import Image from "next/image";
+import Link from "next/link";
+export default function Navbar() {
+  return (
+    <div className="max-ss:w-[410px] w-[1440px] ">
+      <div className=" bg-[#f5f5f5]  h-9 w-[1490px] max-sm:w-auto flex">
+        <div className="ml-12 max-ss:ml-[5px] ">
+        <Image
+          className="pt-2"
+          src={"/Vector3x.png"}
+          alt=""
+          width={24}
+          height={24}
+        />
+        </div>
+
+        <div className="max-md:ml-2 max-ss:ml-[27px] max-sm:mb-1 w-[272px] h-[36px] text-[11px] font-medium  justify-between ml-[1022px] py-2  flex  ">
+          <Link className="relative left-[45px] mb-4  " href="/all-product-page">
+            Find a store |
+          </Link>
+          <Link className="relative left-[45px] " href="/contact">
+            Help |
+          </Link>
+          <Link className="relative left-[45px]  " href="/join-us">
+            Join Us |
+          </Link>
+          <Link className="relative left-[45px]  " href="/sign-in">
+            Sign In 
+          </Link>
+        </div>
+      </div>
+
+      <div className="w-[1440px] h-[60px] flex mt-4 ">
+      <Link className="relative left-[45px] max-ss:ml-[78px]  " href="/">
+
+        <Image
+          className=" "
+          src={"/Vector8.png"}
+          alt=""
+          width={78.47}
+          height={78.47}
+        />
+                    </Link>
+
+        <div className="max-md:hidden w-[1211px] max-ss:hidden h-[36px]  ml-[210px] items-center  justify-between flex ">
+          <div className="max-ss:hidden w-[272px] h-[36px] gap-6  justify-between flex  relative left-[107px] ">
+            <Link className="relative left-[45px]  " href="/all-product-page">
+              {" "}
+              New.&.Featured
+            </Link>
+            <Link className="relative left-[45px] " href="/all-product-page">
+              Men
+            </Link>
+            <Link className="relative left-[45px]  " href="/all-product-page">
+              Women
+            </Link>
+            <Link className="relative left-[45px]  " href="/all-product-page">
+              kids
+            </Link>
+            <Link className="relative left-[45px]  " href="/SignUp">
+              Sale
+            </Link>
+            <Link className="relative left-[45px]  " href="/SignUp">
+              SNKRS
+            </Link>
+          </div>
+          <div className="max-ss:hidden w-[180px] h-[40px] relative left-5 flex ml-[320px] bg-[#f5f5f5] rounded-[10px] ">
+
+            <Image
+              className="pt-[10px] pl-[10px] w-[1390px] max-ss:w-[310px] "
+              src={"/Vector10.png"}
+              alt=""
+              width={24}
+              height={24}
+            />
+
+            <input className="ml-7 bg-[#f5f5f5] rounded-[10px]" placeholder="Search" />
+          </div>
+          <Image
+            className="max-ss:hidden  relative left-5"
+            src={"/Vector.png"}
+            alt=""
+            width={24}
+            height={24}
+          />
+          <Image
+            className="max-ss:hidden relative ml-[-64px] "
+            src={"/Vector (1).png"}
+            alt=""
+            width={24}
+            height={24}
+          />
+        </div>
+
+       
+      </div>
+    </div>
+  );
+}
